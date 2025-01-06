95e23365411ff5131d263ae331b6c12cf00395fc
soikawa <soikawa0108@gmail.com>
2025-01-06 23:59:23 +0900

    テレポート

---
+++ vue/vue-original/5/js/teleport.js
@@ -0,0 +1,25 @@
+Vue
+  .createApp({})
+  .component('my-teleport', {
+    data(){
+      return {
+        show: false
+    }
+  },
+  template: `<form>
+              <input type="button" v-on:click="onclick(true)"
+              value="ダイヤルログを開く" />
+              </form>
+              <teleport to="#popup">
+                <div id="my-dialog" class"dialog" v-if="show">
+                  <p>独自のダイアログです。</p>
+                  <input type="button" v-on:click="onclick(false)" value="閉じる" />
+                </div>
+              </teleport>
+              `,
+  methods:{
+    onclick(flag){
+      this.show = flag
+    }
+  }
+  }).mount('#app');
