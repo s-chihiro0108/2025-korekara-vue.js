b7d2de39106dd0d90f64ebeb16c27494272825cd
soikawa <soikawa0108@gmail.com>
2025-01-14 23:39:01 +0900

    双方向

--- vue/vue-original/5/js/suspense_obj.js
+++ vue/vue-original/5/js/suspense_obj.js
@@ -1,3 +1,26 @@
 Vue
   .createApp({})
-  .component('my-heavy')
+  .component('my-heavy', Vue.defineAsyncComponent({
+    loader(){
+      return new Promise((resole, reject) => {
+        setTimeout(() => {
+          resole({
+            data() {
+              return { name: 'Suspense' };
+            },
+            template: `<div>お待たせしました。{{name}}です！</div>`
+          });
+        }, 5000);
+      })
+    },
+    loadingComponent: {
+      template: `Now Loading...`
+    },
+    delay: 500,
+    errorComponent: {
+      template: `Loading Error!!`
+    },
+    timeout: 7000,
+    suspensible: false,
+  }))
+  .mount('#app')
