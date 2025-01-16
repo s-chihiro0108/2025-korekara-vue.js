1e47022953e5e03bbbcc223ff308df830b511e25
soikawa <soikawa0108@gmail.com>
2025-01-17 00:12:15 +0900

    6-2-1

---
+++ vue/vue-original/6/js/setup_watch.js
@@ -0,0 +1,31 @@
+Vue
+  .createApp({
+    data(){
+      return {
+        init: 0
+      };
+    },
+    methods: {
+      onclick(){
+        this.init = Math.floor(Math.random()* 100 ) + 1;
+      }
+    }
+  })
+  .component('my-counter', {
+    props: ['init'],
+    template: `<div>現在値は{{ current }}です！
+      <input type="button" v-on:click="onclick" value="増やす" /></div>`,
+    setup(props){
+      const current = Vue.ref(props.init);
+      Vue.watch(
+        () => props.init,
+        (newV, oldV) => {
+          current.value = newV;
+        }
+      );
+      const onclick = () => {
+        current.value++;
+      };
+      return { current, onclick };
+    }
+  }).mount('#app');
